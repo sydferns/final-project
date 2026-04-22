c23ccf21427b935dd2d4fb5069a407ca273130e4
Sydelle <sferns0613@gmail.con>
2026-04-21 21:00:07 -0500

    script to set up demo screen

---
+++ outline.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("BTS")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
